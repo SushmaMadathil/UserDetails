01299dec04026d740ff2b3e0e6cbed9fb2a97e25
sushmakayem <37783638+sushmakayem@users.noreply.github.com>
2018-10-15 20:11:33 -0500

    First commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root"
+})
+export class DataService {
+  //dependency interjection
+  constructor(private http: HttpClient) {}
+
+  getUsers() {
+    return this.http.get("https://jsonplaceholder.typicode.com/users");
+  }
+
+  getUser(userID) {
+    return this.http.get(
+      "https://jsonplaceholder.typicode.com/users/" + userID
+    );
+  }
+
+  getPosts() {
+    return this.http.get("https://jsonplaceholder.typicode.com/posts");
+  }
+}
